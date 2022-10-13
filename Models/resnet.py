5c4db491b80a3dc9f00b33bdefb2fbe97634f1b3
DorianJoubaud <djoubaud@gmail.com>
2022-10-13 12:13:28 +0200

    Wandb log lr

--- Models/resnet.py
+++ Models/resnet.py
@@ -105,7 +105,8 @@ class RESNET:
       self.initial_learning_rate = initial_learning_rate
 
     def __call__(self, step):
-      return float(self.initial_learning_rate / (step + 1))
+
+      return float(self.initial_learning_rate / float((step + 1)))
 
   class LRLogger(tf.keras.callbacks.Callback):
     def __init__(self, optimizer):
